a9a323fa48bc32855f394a884ab281e33ecabb49
Dingadung <1229jimin@sju.ac.kr>
2023-10-05 22:23:49 +0900

    Study: ReactJS로 영화 웹 서비스 만들기 section 7.2 Coin Tracker

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,38 +1,25 @@
+// import { response } from 'express';
 import './App.css';
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  }
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange}
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do...">
-        </input>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-      {toDos.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-      </ul>
+      <h1>The Conins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : (<ul>
+        {coins.map((coin) => <li>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</li>)}
+      </ul>)}
+      
     </div>
   );
 }
